a59d028e7c38423d516b2e9705144b4962766028
Ahmed Khaled <91345253+xSW1TCHx@users.noreply.github.com>
2022-12-15 17:34:37 +0200

    I hate this

--- XML Editor/src/File.java
+++ XML Editor/src/File.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 
 public class File {
 	
-	public static ArrayList<String> checkConsistency(ArrayList<String> XML) {
+	public static ArrayList<String> correct(ArrayList<String> XML) {
 		int lineIndex = 0;
 		String line;
 		Stack<Tag> tags = new Stack<Tag>();
@@ -58,23 +58,20 @@ public class File {
 								if(mismatchingTagsF == true) {
 									
 									int i = 1;
-									while(line.charAt(i-1) != '>') {
+									while(line.charAt(i-1) != '<') {
 										i++;
 									}
-									if(line.charAt(i) == '1'||line.charAt(i) == '2'||line.charAt(i) == '3'||line.charAt(i) == '4'||line.charAt(i) == '5'||line.charAt(i) == '6'||line.charAt(i) == '7'||line.charAt(i) == '8'||line.charAt(i) == '9'||line.charAt(i) == '0') {
-										String correctLine = " ".repeat(tags.peek().margin) + "<id>" + line.charAt(i) + "</id>";
-										XML.set(lineIndex, correctLine);
+									int j = i;
+									while(line.charAt(j) != '>') {
+										j++;
 									}
-									else {
-										int j = i;
-										while(line.charAt(j+1) != '<') {
-											j++;
-										}
-										String name = line.substring(i, j);
-										String correctLine = " ".repeat(tags.peek().margin) + "<name>" + name + "</name>";
-										XML.set(lineIndex, correctLine);
+									String mismatchingTagLabel = line.substring(i,j);
+									int k = j;
+									while(line.charAt(k) != '<') {
+										k++;
 									}
-									mistakes.add("Mismatching tags in line " + (tags.peek().line+1) + "\n");
+									XML.set(lineIndex, line.substring(0,k) + "</" + mismatchingTagLabel + ">");
+									mistakes.add("Mismatching tags for <" + (tags.peek().label) + ">" + " in line " + (tags.peek().line+1));
 									tags.pop();
 								}
 								else if(noClosingTagF == true) {
@@ -82,62 +79,42 @@ public class File {
 									noOpeningTagF = false;
 									while(!noClosingTags.isEmpty()) {
 										Tag noClosingTag = noClosingTags.remove();
-										String afterLine = XML.get(noClosingTag.line+1);
 										String closingTag = "</" + noClosingTag.label + ">";
-										int afterLineMargin = 0;
-										while(afterLine.charAt(afterLineMargin) == ' ') {
-											afterLineMargin++;
-										}
-										if(noClosingTag.margin >= afterLineMargin) {
-											XML.set(noClosingTag.line, XML.get(noClosingTag.line) + closingTag);
-										}
-										else if(noClosingTag.margin < afterLineMargin){
-											int i = 1;
-											while(!XML.get(noClosingTag.line+i).equals(" ".repeat(noClosingTag.margin))) {
-												i++;
-											}
-											XML.set(noClosingTag.line+i, XML.get(noClosingTag.line+i) + closingTag);
-										}
-										mistakes.add("No closing tag for " + noClosingTag.label + " in line " +  (noClosingTag.line+1) + "\n");
+										XML.set(noClosingTag.line, XML.get(noClosingTag.line) + closingTag);
+										mistakes.add("No closing tag for <" + noClosingTag.label + "> in line " +  (noClosingTag.line+1));
 										tags.pop();
 									}
 								}
 								else if(noOpeningTagF == true) {
 									String myLine = XML.get(lineIndex);
-									String beforeLine = XML.get(lineIndex-1);
 									int myLineMargin = 0;
 									
 									while(myLine.charAt(myLineMargin) == ' ') {
 										myLineMargin++;
 									}
-									int beforeLineMargin = 0;
-									while(beforeLine.charAt(beforeLineMargin) == ' ') {
-										beforeLineMargin++;
-									}
-									if(myLineMargin >= beforeLineMargin) {
-										String margin = myLine.substring(0, myLineMargin);
-										String restOfLine = myLine.substring(myLineMargin, myLine.length());
-										myLine = margin + "<" + closingTagLabel + ">" + restOfLine;
-										XML.set(lineIndex, myLine);
-									}
-									else if(myLineMargin < beforeLineMargin) {
-										int i = lineIndex;
-										while(!XML.get(lineIndex-i).equals(" ".repeat(myLineMargin))) {
-											i--;
-										}
-										XML.set(lineIndex-i, XML.get(lineIndex-i) + "<" + closingTagLabel + ">");
-									}
 									
-									mistakes.add("No opening tag for " + closingTagLabel + " in line " +  (lineIndex+1) + "\n");
+									String margin = myLine.substring(0, myLineMargin);
+									String restOfLine = myLine.substring(myLineMargin, myLine.length());
+									myLine = margin + "<" + closingTagLabel + ">" + restOfLine;
+									XML.set(lineIndex, myLine);
+									mistakes.add("No opening tag for </" + closingTagLabel + "> in line " +  (lineIndex+1));
 								}
 								
 							}
 						}
 						else {
-							String correctLine = XML.get(XML.size()-lineIndex-1);
-							correctLine += "<" + closingTagLabel + ">";
-							XML.set(XML.size()-lineIndex-1, correctLine);
-							mistakes.add("No opening tag for " + closingTagLabel + " in line " +  (lineIndex+1) + "\n");
+							String myLine = XML.get(0);
+							int myLineMargin = 0;
+							
+							while(myLine.charAt(myLineMargin) == ' ') {
+								myLineMargin++;
+							}
+							
+							String margin = myLine.substring(0, myLineMargin);
+							String restOfLine = myLine.substring(myLineMargin, myLine.length());
+							myLine = margin + "<" + closingTagLabel + ">" + restOfLine;
+							XML.set(0, myLine);
+							mistakes.add("No opening tag for </" + closingTagLabel + "> in line " +  (lineIndex+1));
 						}
 					}
 					else {
@@ -161,22 +138,10 @@ public class File {
 			Tag noClosingTag = tags.pop();
 			String correctLine = " ".repeat(noClosingTag.margin) + "</" + noClosingTag.label + ">";
 			XML.add(correctLine);
-			mistakes.add("No closing tag for" + noClosingTag.label + "\n");
+			mistakes.add("No closing tag for <" + noClosingTag.label + "> " + "in line " + (noClosingTag.line+1));
 		}
 		return mistakes;
 		
 	}
 	
-}
-
-class Tag {
-	String label;
-	int line;
-	int margin;
-	public Tag(String label, int line, int margin) {
-		super();
-		this.label = label;
-		this.line = line;
-		this.margin = margin;
-	}
 }
---
+++ XML Editor/src/Tag.java
@@ -0,0 +1,13 @@
+
+public class Tag {
+	String label;
+	int line;
+	int margin;
+	public Tag(String label, int line, int margin) {
+		super();
+		this.label = label;
+		this.line = line;
+		this.margin = margin;
+	}
+}
+
